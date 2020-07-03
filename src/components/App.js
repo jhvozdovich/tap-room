a88166c3c2ce6aedfba55371a8fca1d2bb0c96fd
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-07-02 20:35:55 -0700

    add header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from './Header';
 
 
 function App() {
   return (
-    <h1>Test</h1>
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <React.Fragment>
+      <h1>Witchy Brews</h1>
+      <h4>For all your adventuring needs!</h4>
+    </React.Fragment>
+  )
+}
+
+export default Header;
